44f02be40bea16682a26c1f90fbf1ae0aa5678c5
Sergey Gribovski <sgribovski@gmail.com>
2017-04-28 13:56:37 +0200

    refactores combiner

--- lib/combiner.rb
+++ lib/combiner.rb
@@ -5,37 +5,18 @@
 # output:
 # - enumerator for the combined elements
 class Combiner
-  def initialize(&key_extractor)
-    @key_extractor = key_extractor
+  def initialize(&extractor)
+    @extractor = extractor
   end
 
   def combine(*enumerators)
+    @enumerators = enumerators
+
     Enumerator.new do |yielder|
-      last_values = Array.new(enumerators.size)
-      done = enumerators.none?
-      while not done
-        last_values.each_with_index do |value, index|
-          if value.nil? && !enumerators[index].nil?
-            begin
-              last_values[index] = enumerators[index].next
-            rescue StopIteration
-              enumerators[index] = nil
-            end
-          end
-        end
-
-        done = enumerators.none? && last_values.none?
-        unless done
-          min_key = last_values.map { |val| key(val) }.compact.min
-          values = Array.new(last_values.size)
-          last_values.each_with_index do |value, index|
-            if key(value) == min_key
-              values[index] = value
-              last_values[index] = nil
-            end
-          end
-
-          yielder.yield(values)
+      while values.any?
+        yielder.yield iteration_values
+        iteration_values.each_with_index do |value, index|
+          enumerators[index].next if value
         end
       end
     end
@@ -43,9 +24,22 @@ class Combiner
 
   private
 
-  attr_reader :key_extractor
+  attr_reader :extractor, :enumerators
 
-  def key(value)
-    value.nil? ? nil : @key_extractor.call(value)
+  def values
+    enumerators.map do |enum|
+      begin
+        enum.peek
+      rescue StopIteration
+        nil
+      end
+    end
+  end
+
+  def iteration_values
+    min_value = values.compact.min do |a, b|
+    	extractor.call(a) <=> extractor.call(b)
+    end
+    values.map {|value| value == min_value ? value : nil}
   end
 end
